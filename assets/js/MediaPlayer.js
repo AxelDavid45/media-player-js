cbaadf91b6c74b87ffb69c9afc57a02779d73370
Axel Espinosa <axeldavid45@gmail.com>
2020-06-23 00:22:07 -0500

    AutoPlayer Added as a plugin

--- assets/js/MediaPlayer.js
+++ assets/js/MediaPlayer.js
@@ -1,6 +1,16 @@
 function MediaPlayer(configs) {
     this.video = configs.src;
+    this.plugins = configs.plugins || [];
+    this._initPlugins();
 }
+
+MediaPlayer.prototype._initPlugins = function () {
+    if (this.plugins.length > 0) {
+        this.plugins.forEach(plugin => {
+           plugin.run(this);
+        });
+    }
+};
 
 MediaPlayer.prototype.play = function () {
     if (!this.video.paused) {
@@ -9,5 +19,20 @@ MediaPlayer.prototype.play = function () {
         this.video.play();
     }
 };
+
+MediaPlayer.prototype.paused = function () {
+    this.video.pause();
+};
+
+MediaPlayer.prototype.start = function () {
+    if (this.video.paused) {
+        this.play();
+    } else {
+        this.play();
+    }
+};
+MediaPlayer.prototype.mute = function () {
+    this.video.muted = true;
+}
 
 export default MediaPlayer;
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,10 @@
 import MediaPlayer from "./MediaPlayer.js";
+import AutoPlay from './plugins/AutoPlay.js';
 const videoSrc = document.querySelector('video');
 const button = document.querySelector('button');
-const configs = {src: videoSrc};
+const configs = {src: videoSrc, plugins: [new AutoPlay()]};
 const player = new MediaPlayer(configs);
 
 button.onclick = async () => {
-    try {
-        await player.play();
-    } catch (error) {
-        console.error(error);
-    }
+    await player.start();
 };
